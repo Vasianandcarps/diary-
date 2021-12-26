bbf737ed2f75b767eeb1d7bdb995d0fe059a13dc
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2021-12-26 14:10:15 +0200

    Update diary.js

--- diary.js
+++ diary.js
@@ -2,17 +2,21 @@ function Record(date,recording){
     this.date = date;
     this.recording = recording;
     this.toString = function (){
-       return document.write(`${this.date} <br> ${this.recording}`);
+       return `${this.date} <br> ${this.recording}`;
     }
 }
 function Diary(){
-    this.diary = ["h"] ;
+    this.diary = [] ;
     this.showDiary = function (){
         this.diary.forEach(element => document.write(element));
     },
-    this.addRecord = function (){
-
+    this.addRecord = function (rec){
+        this.diary.unshift(rec.toString())
     }
-
 }
-
+let diary = new Diary();
+function newNote(){
+    let note = new Record(new Date(), prompt("input your note"));
+    
+    diary.addRecord(note);
+}
