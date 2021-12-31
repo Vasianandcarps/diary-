b354013b773d9d8a1980364c31683345ab0ebdbb
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2021-12-31 15:01:46 +0200

    Update diary.js

--- diary.js
+++ diary.js
@@ -1,34 +1,45 @@
-function Record(date,recording){
-    this.date = date;
-    this.recording = recording;
-    this.toString = function (){
-       return `${this.date} <br> ${this.recording}`;
-    }
+function Record(date, recording, notes) {
+  this.date = date;
+
+  this.notes = notes;
+  this.recording = recording;
+  this.toString = function () {
+    return `${this.date} <br> ${this.recording}`;
+  };
 }
-function Diary(){
-    this.diary = [] ;
-    this.showDiary = function (){
-        this.diary.forEach(element => document.write(`${element} <br>` ));
-    },
-    this.addRecord = function (rec){
-        this.diary.unshift(rec.toString())
+function Diary() {
+  this.diary = [];
+  (this.showDiary = function () {
+    this.diary.forEach((element) => (notes.innerHTML += `${element} <br>`));
+  }),
+    (this.addRecord = function (rec) {
+      this.diary.unshift(rec.toString());
+    });
+  (this.search = function () {
+    let n = prompt("input expression or world");
+
+    result = this.diary.filter((word) => (word.includes(n) ? true : false));
+    if (result.length > 0) {
+      result.forEach((element) => (notes.innerHTML += `${element}  `));
+    } else {
+      notes.innerHTML +=
+        "You dont have this expression or word in your notes.  " + "<br>";
     }
-    this.search = function (){
-        // let search = prompt("input expression or world") ;
-        let result = this.diary.filter(word =>word.includes(prompt("input expression or world"))?true:false);
-        result.forEach(element => document.write(`${element} <br>` ));
-    }
-    ,
-    this.weekend = function (){
-        let result = this.diary.filter(word => word.includes("Sat")?true:word.includes("Sun")?true: "You dont have weekend notes");
-        result.forEach(element => document.write(`${element} <br>` ));
-    }
+  }),
+    (this.weekend = function () {
+      let result = this.diary.filter((word) =>
+        word.startsWith("Sat") ? true : word.startsWith("Sun") ? true : false
+      );
+      if (result.length > 0) {
+        result.forEach((element) => (notes.innerHTML += `${element}  `));
+      } else {
+        notes.innerHTML += "You dont have weekend notes " + "<br>";
+      }
+    });
 }
 let diary = new Diary();
-function newNote(){
-    let note = new Record(new Date(), prompt("input your note"));
-    
-    diary.addRecord(note);
+function newNote() {
+  let note = new Record(new Date(), prompt("input your note"));
+
+  diary.addRecord(note);
 }
-
-
